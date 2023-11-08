e78d80a8888f27a1b89af033e597a53f82de1874
GalisGalisteo <galis.travelling@gmail.com>
2023-11-08 10:55:21 +0100

    finishing ex with computed

--- js/app.js
+++ js/app.js
@@ -3,13 +3,32 @@
 import {
   createApp,
   ref,
+  computed
 } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   setup() {
-    const message = ref("Hello Vue!");
+    const counter = ref(0);
+    const addCount = () => {
+      counter.value++;
+    };
+    const lowerCount = () => {
+      counter.value--;
+    };
+
+    const updateColor = computed(() => {
+      if (counter.value < 0) {
+        return "text-danger";
+      } else if (counter.value > 0) {
+        return "text-success";
+      }
+    });
+
     return {
-      message,
+      counter,
+      addCount,
+      lowerCount,
+      updateColor,
     };
   },
 }).mount("#app");
