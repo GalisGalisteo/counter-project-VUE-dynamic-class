a119ef9d6333fdf0aa29bf8d83ba2661eb2b798d
GalisGalisteo <galis.travelling@gmail.com>
2023-11-09 11:13:56 +0100

    updating

--- js/app.js
+++ js/app.js
@@ -3,31 +3,20 @@
 import {
   createApp,
   ref,
-  computed
+  computed,
 } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   setup() {
     const counter = ref(0);
-    const addCount = () => {
-      counter.value++;
-    };
-    const lowerCount = () => {
-      counter.value--;
-    };
 
     const updateColor = computed(() => {
-      if (counter.value < 0) {
-        return "text-danger";
-      } else if (counter.value > 0) {
-        return "text-success";
-      }
+      if (counter.value < 0) return "text-danger";
+      if (counter.value > 0) return "text-success";
     });
 
     return {
       counter,
-      addCount,
-      lowerCount,
       updateColor,
     };
   },
